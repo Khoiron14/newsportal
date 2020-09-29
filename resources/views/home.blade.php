0a93e881267dc1edba531bd4753610c721658578
Khoiron14 <muh.khoiron14@gmail.com>
2020-09-29 17:32:07 +0700

    show post image on list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,9 @@
             @foreach($posts as $post)
                 <div class="card mb-3">
                     <div class="card-body">
+                        @if ($post->image != null)
+                        <img src="{{ URL::to('/') }}/images/{{ $post->image->url }}" class="mb-3 mr-3 float-left" width="200px" alt="{{ $post->slug }}">
+                        @endif
                         <a href="{{ route('posts.show', $post) }}"><h5>{{ $post->title }}</h5></a>
                         {{ $post->contentLimit() }}...
                         <br>
